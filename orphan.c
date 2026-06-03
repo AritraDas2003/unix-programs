ebef2cb60b55503c81d833118e2df2e09df3bdb6
Aritra Das <aritragamer007@gmail.com>
2026-06-03 23:40:13 +0530

    small mistake in the parent process pid

--- orphan.c
+++ orphan.c
@@ -21,7 +21,7 @@ int main() {
 	else {
 		//child process 
 		printf("Child Process: PID = %d\n", getpid());
-		printf("Initial Parent PID = %d\n", getpid());
+		printf("Initial Parent PID = %d\n", getppid());
 		sleep(5);
 		printf("After parent exit...\n");
 		printf("Now Parent PID = %d\n", getppid());
